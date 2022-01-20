0b7730579416881282ced786a705364170747b41
ismail <iduzel49@gmail.com>
2022-01-20 15:14:17 +0100

    single card added

--- src/pages/CardDetails.js
+++ src/pages/CardDetails.js
@@ -1,28 +1,39 @@
-import React, { useContext } from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import DataContext from '../redux/dataContext';
+import React, { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import Demo from "../Demo";
+import DataContext from "../redux/dataContext";
+import './CardDetail.css';
 
 function CardDetails(params) {
+  const { id } = useParams();
+  const cardContext = useContext(DataContext);
+  const [currentItem, setCurrentItem] = useState(cardContext?.data?.filter((item) => item.id.toString() === id));
 
-    const {id}= useParams()
-    const cardContext = useContext(DataContext)
-    const [currentItem, setCurrentItem] = useState(null);
+ 
 
-    useEffect(()=>{
-     if(id!== undefined){
-      setCurrentItem(cardContext?.data?.filter(item => item.id.toString() ===id))
-     }
-      
-      
-    },[id])
-  return <div>
-    <h1>{id}</h1>
+  console.log(JSON.stringify(currentItem));
 
-    <p>{JSON.stringify(currentItem)}</p>
+  return (
+    <div className="card-details container">
+      <div className="img left">
+          <img className="img" src={currentItem[0].image} />
+      </div>
 
-  </div>;
+      <div className="text right">
+          <h2 className="title">{currentItem[0].title}</h2>
+          <h2 className="price">$ {currentItem[0].price}</h2>
+          <p className="category">Category:   {currentItem[0].category}</p>
+          <p>Availability:  In Stock</p>
+          <p className="line"></p>
+          <p className="description">{currentItem[0].description}</p>
+           <div className="buttons addCart">
+               
+           </div>
+      </div>
+    </div>
+  );
 }
 
 export default CardDetails;
